05c17f711c2d22980373b19bd8a42bbb17c19810
李中梁 <keloli@163.com>
2018-01-16 13:05:29 +0800

    Update Regressor.py

--- Regressor.py
+++ Regressor.py
@@ -1,14 +1,15 @@
 # Regressor example
-# https://github.com/keloli/KerasPractise/edit/master/Regressor.py  
+# https://github.com/keloli/KerasPractise/edit/master/Regressor.py
 
 import numpy as np
 np.random.seed(1337)  # for reproducibility
-from keras.models import Sequential
+from keras.models import Sequential # 一个层一个层增加的线性模型
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 
 # create some data
 X = np.linspace(-1, 1, 200)
+# print (X)
 np.random.shuffle(X)    # randomize the data
 Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, ))
 # plot data
@@ -21,7 +22,8 @@ X_test, Y_test = X[160:], Y[160:]       # last 40 data points
 # build a neural network from the 1st layer to the last layer
 model = Sequential()
 
-model.add(Dense(units=1, input_dim=1)) 
+model.add(Dense(output_dim=1, input_dim=1)) # Dense是全连接层，第一层需要定义输入，
+											# 第二层只需要定义输出，第二层一第一层的输出作为输入
 
 # choose loss function and optimizing method
 model.compile(loss='mse', optimizer='sgd')
@@ -37,7 +39,9 @@ for step in range(301):
 print('\nTesting ------------')
 cost = model.evaluate(X_test, Y_test, batch_size=40)
 print('test cost:', cost)
-W, b = model.layers[0].get_weights()
+W, b = model.layers[0].get_weights()	# 查看训练出的网络参数
+										# 由于我们网络只有一层，且每次训练的输入只有一个，输出只有一个
+										# 因此第一层训练出Y=WX+B这个模型，其中W,b为训练出的参数
 print('Weights=', W, '\nbiases=', b)
 
 # plotting the prediction
