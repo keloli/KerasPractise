a313bc8e455f51944cb96e7ee3d9946c259907e5
keloli <keloli@163.com>
2018-01-18 13:09:58 +0800

    new file

---
+++ SaveLoadModel.py
@@ -0,0 +1,38 @@
+# _*_ coding :utf-8 _*_
+# save&load example
+# 要安装HDF5保存为`.h5`格式 `pip install h5py`
+
+# 以训练回归模型为例子
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
+
+# create some data
+X = np.linspace(-1, 1, 200)
+np.random.shuffle(X)    # randomize the data
+Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, ))
+X_train, Y_train = X[:160], Y[:160]     # first 160 data points
+X_test, Y_test = X[160:], Y[160:]       # last 40 data points
+model = Sequential()
+model.add(Dense(output_dim=1, input_dim=1))
+model.compile(loss='mse', optimizer='sgd')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+
+
+# save
+print('test before save: ', model.predict(X_test[0:2]))
+model.save('my_model.h5')   # HDF5 file, you have to pip3 install h5py if don't have it
+del model  # deletes the existing model
+
+# load
+model = load_model('my_model.h5')
+print('test after load: ', model.predict(X_test[0:2]))
+
+
+
+
+
